9b009ef1d76d39d80ef36c5f1eb85dd684b982b8
SultonMuhajir <sulton.muhajir28@gmail.com>
2021-02-12 15:53:08 +0700

    portfolio v1.3
    penyempurnaan website

--- js/script.js
+++ js/script.js
@@ -24,15 +24,22 @@ $("nav li a").on("click", function () {
 
 // => Form Validation
 function validation() {
-   // const alp = /^[a-zA-Z ]+$/;
-   // const num = /^[0-9]+$/;
-   // const email = /^\w+[\+\.\w-]*@([\w-]+\.)*\w+[\w-]*\.([a-z]{2,4}|\d+)$/i;
+   const alp = /^[a-zA-Z ]+$/;
+   const num = /^[0-9]+$/;
+   const mail = /^\w+[\+\.\w-]*@([\w-]+\.)*\w+[\w-]*\.([a-z]{2,4}|\d+)$/i;
    const fname = document.getElementById("fname");
+   const lname = document.getElementById("lname");
+   const phone = document.getElementById("phone");
+   const email = document.getElementById("email");
    const message = document.getElementById("message");
    const errFname = document.getElementById("errFname");
+   const errLname = document.getElementById("errLname");
+   const errPhone = document.getElementById("errPhone");
+   const errEmail = document.getElementById("errEmail");
    const errMes = document.getElementById("errMes");
    const name = fname.value;
 
+   // First Name
    if (fname.value == "") {
       errFname.innerHTML = "* Required";
       fname.focus();
@@ -41,6 +48,78 @@ function validation() {
       errFname.innerHTML = "";
    }
 
+   if (!fname.value.match(alp)) {
+      errFname.innerHTML = "* Invalid";
+      fname.focus();
+      return false;
+   } else {
+      errFname.innerHTML = "";
+   }
+
+
+   // Last Name
+   if (lname.value == "") {
+      errLname.innerHTML = "* Required";
+      lname.focus();
+      return false;
+   } else {
+      errLname.innerHTML = "";
+   }
+
+   if (!lname.value.match(alp)) {
+      errLname.innerHTML = "* Invalid";
+      lname.focus();
+      return false;
+   } else {
+      errLname.innerHTML = "";
+   }
+
+
+   // Phone
+   if (phone.value == "") {
+      errPhone.innerHTML = "* Required";
+      phone.focus();
+      return false;
+   } else {
+      errPhone.innerHTML = "";
+   }
+
+   if (!phone.value.match(num)) {
+      errPhone.innerHTML = "* Invalid";
+      phone.focus();
+      return false;
+   } else {
+      errPhone.innerHTML = "";
+   }
+
+   if (phone.value.length < 10 || phone.value.length > 13) {
+      errPhone.innerHTML = "* Invalid";
+      phone.focus();
+      return false;
+   } else {
+      errPhone.innerHTML = "";
+   }
+
+
+   // Email
+   if (email.value == "") {
+      errEmail.innerHTML = "* Required";
+      email.focus();
+      return false;
+   } else {
+      errEmail.innerHTML = "";
+   }
+
+   if (!email.value.match(mail)) {
+      errEmail.innerHTML = "* Invalid";
+      email.focus();
+      return false;
+   } else {
+      errEmail.innerHTML = "";
+   }
+
+
+   // Message
    if (message.value == "") {
       errMes.innerHTML = "* Required";
       message.focus();
@@ -52,6 +131,11 @@ function validation() {
          'Your Message Has Been Received',
          'success'
       )
+      fname.value = "";
+      lname.value = "";
+      phone.value = "";
+      email.value = "";
+      message.value = "";
       return false;
    }
 }
@@ -59,14 +143,6 @@ function validation() {
 
 
 // => Sweet Alert
-document.querySelector(".About button").addEventListener("click", function () {
-   Swal.fire(
-      'Under Construction',
-      '',
-      'info'
-   )
-});
-
 document.querySelector(".Skills button").addEventListener("click", function () {
    Swal.fire(
       'Under Construction',
